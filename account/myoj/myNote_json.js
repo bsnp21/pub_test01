a8f89a0021712ceef0b579204624c2dae0bb2724
wdingsoft <wdingbox@gmail.com>
2021-01-21 12:52:48 -0500

    Sync:_myNote~Gen1:1 saved.. repodesc:

--- account/myoj/myNote_json.js
+++ account/myoj/myNote_json.js
@@ -1,7 +1,7 @@
 var _Max_struct={
     "Gen": {
         "1": {
-            "1": "<ol><li>abc bb loc-cal</li></ol>",
+            "1": "<ol><li>abc bb loc-cal--tst</li></ol>",
             "2": "",
             "3": "",
             "4": "",
